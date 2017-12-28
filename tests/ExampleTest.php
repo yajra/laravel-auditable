27d29a445d7d97802a25bafea951ac1a000586e9
Arjay Angeles <aqangeles@gmail.com>
2017-12-28 12:45:40 +0800

    Fix example test.

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,7 +2,9 @@
 
 namespace Yajra\Auditable\Test;
 
-class ExampleTest extends \PHPUnit_Framework_TestCase
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
 {
     /**
      * Test that true does in fact equal true
