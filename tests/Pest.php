bcca6e7803dc90ceb1a40b3ed71f5b8d8736bc13
Arjay Angeles <aqangeles@gmail.com>
2024-03-17 12:27:54 +0800

    chore: use pest

--- tests/Pest.php
+++ tests/Pest.php
@@ -27,9 +27,7 @@ uses(TestCase::class, RefreshDatabase::class)->in('Feature');
 |
 */
 
-expect()->extend('toBeOne', function () {
-    return $this->toBe(1);
-});
+expect()->extend('toBeOne', fn () => $this->toBe(1));
 
 /*
 |--------------------------------------------------------------------------
